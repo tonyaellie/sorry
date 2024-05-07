11ca6b9a91b72763e72a6763c4cef04d29fc9d02
tonyaellie <tonya@tokia.dev>
2024-05-07 17:50:34 +0000

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 import { TRPCReactProvider } from "~/trpc/react";
 import { Toaster } from "~/components/ui/sonner";
@@ -24,6 +25,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>
+        <Script
+          defer
+          data-domain="sorry.tokia.dev"
+          src="https://ingest.tokia.dev/js/script.js"
+        />
         <TRPCReactProvider>{children}</TRPCReactProvider>
         <Toaster />
       </body>
